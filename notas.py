fff5b776d7d11334e1b42460f09534006c9fbca5
toni <toni@rxn.com>
2020-09-10 14:23:54 -0500

    con curso de git

--- notas.py
+++ notas.py
@@ -1,3 +1,23 @@
 # token visual estudio
 #  7ac70123c7ff05880e39995bb1e498366662c8dc  ------>    Acces personal token
 #  e1114b94b8f255668cc30e2e5bdcd930  --------> github-gits 
+
+
+# git and github
+
+
+# git init ---->  iniciar git
+# git status ----->  ver estado del los archivos
+# git add nomArchivo or git add . -----> agregar un archivo a git y tambien para agregar cambios a un archivo.
+# git commit ----->  crear snapshoot a los archivos y tambien para aceptar y guardar los cambios .
+# git commit -m "mensaje" ---> coomit y mensaje a la vez. 
+# git log ----->  informacion de los commit
+# git checkout nomArchivo  ------>  descartar cambios en el archivo.
+# git diff nomArchivo  ----->  observar las diferencias entre el actual y el anterior archivo.
+# crear un archivo que se llame (.gitignore) - luego colocar los archivos a ignorar alli.
+# git branch --> observar versiones.
+# git branch nomVersion  ----> Agregar una nueva version.
+# git checkout nomVersion -----> cambiar a otra version.
+# 
+
+
